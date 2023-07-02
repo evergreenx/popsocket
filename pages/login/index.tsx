19e07c5dc99bf5caa34cf97e29726d4a635bb1da
ido evergeen <dicksonevegreenido@gmail.com>
2023-07-02 20:43:26 +0100

    add google auth

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -9,6 +9,17 @@ export default function Login() {
   return (
     <AuthPage
       type="login"
+      providers={[
+
+
+
+
+
+        {
+          name: "google",
+          label: "Sign in with Google",
+        },
+      ]}
       renderContent={(content: React.ReactNode, title: React.ReactNode) => {
         return (
           <div className="">
